1b11c3158b6daca2a77980a8dd42c4c68576faf6
Jayesh Bhadja <kailashjayesh@live.com>
2025-04-25 02:43:47 +0530

    Update string_calculator.rb #add method when input is a single number as string - Convert to integer and return as single number present in string. - Unit Test Passed

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,9 @@ class StringCalculator
   # Returns the sum of those numbers as an integer
   def add(numbers)
     # If the input string is empty, return 0
-    0 if numbers.empty?
+    return 0 if numbers.empty?
+
+    # convert to integer and return
+    numbers.to_i
   end
 end
