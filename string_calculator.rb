12aacc53ca547efbacc26f61d7d7e13235d4ecc4
Jayesh Bhadja <kailashjayesh@live.com>
2025-04-25 03:05:12 +0530

    Refactor string_calculator.rb #add method - Split the string by commas - Convert each string in the array to an integer and calculate the total sum using inject - Unit Test Passed

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,7 +5,11 @@ class StringCalculator
     # If the input string is empty, return 0
     return 0 if numbers.empty?
 
-    # Split the string by commas, convert each part to integer, and sum them
-    numbers.split(',').map(&:to_i).sum
+    # Split the comma-separated string 'numbers' into an array of strings
+    array_of_strings = numbers.split(',')
+
+    # Convert each string in the array to an integer and calculate the total sum
+    # Using 'inject' to iterate through the array and accumulate the sum
+    array_of_strings.inject(0) { |sum, num| sum + num.to_i }
   end
 end
